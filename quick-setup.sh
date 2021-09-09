eceefcf4895ba5d912ca7f423be19039d15aed57
Kihau <michal.haldas@gmail.com>
2021-09-09 23:26:53 +0200

    Update quick-setup

--- quick-setup.sh
+++ quick-setup.sh
@@ -1,2 +1,15 @@
 # Used to quickly load all config files
 # Overridess existing config files with a new ones
+
+
+# If config directory doesn't, exist create one.
+if [ -d "~/.config" ]; then
+	mkdir ~/.config
+fi
+
+# Replace bash config files with new ones
+cp bashrc ~/.bashrc
+cp bash_aliases ~/.bash_aliases
+
+# Replace/Create all config files
+cp -r config ~/.config
